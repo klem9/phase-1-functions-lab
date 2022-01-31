823ba27d5cfde03c37816979c4a3b8292b8fc853
Klemense <klemenseloh@gmail.com>
2022-01-31 17:01:54 -0500

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solution in this file!
+let hq = 42;
+
+function distanceFromHqInBlocks(distanceValue){
+    return Math.abs(hq-distanceValue);
+}
+
+function distanceFromHqInFeet(distanceValue){
+    return distanceFromHqInBlocks(distanceValue) * 264;
+}
+
+function distanceTravelledInFeet(start,destination){
+    return Math.abs(start-destination) * 264;
+}
+
+function calculatesFarePrice(start,destination){
+    if(distanceTravelledInFeet(start,destination) <= 400) {
+        return 0;
+    } else if (distanceTravelledInFeet(start,destination) <= 2000) {
+        return (distanceTravelledInFeet(start,destination) -400) * 0.02;
+    } else if (distanceTravelledInFeet(start,destination) <= 2500) {
+        return 25;
+    } else {
+        return "cannot travel that far";
+    }
+}
